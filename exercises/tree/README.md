caf4ece1f96864ad495edd9a9b1b13e758589dc1
ryandadeng <ryandadeng@gmail.com>
2019-01-06 23:06:16 +1100

    adedd

---
+++ exercises/tree/README.md
@@ -0,0 +1,13 @@
+# Traversal
+
+## Breadth-First Traversal
+
+- Start on each level from root
+- Each level start from left to right
+
+
+
+## Depth-First Traversal
+
+- start on root
+- go from left node first and keep searching for its child
--- exercises/tree/index.js
+++ exercises/tree/index.js
@@ -10,8 +10,30 @@
 // on the tree class.  Each method should accept a
 // function that gets called with each element in the tree
 
-class Node {}
+class Node {
+    constructor(data) {
+        this.data = data;
+        this.children = [];
+    }
 
-class Tree {}
+    add(data) {
+        this.children.push(new Node(data));
+    }
 
-module.exports = { Tree, Node };
+    remove(data) {
+        this.children.splice()
+    }
+}
+
+class Tree {
+
+    traverseBF() {
+
+    }
+
+    traverseDF() {
+
+    }
+}
+
+module.exports = {Tree, Node};
